6eddda285180f9209057b26cbaa5a8a50d4fd1f0
Mischa Tobias <mischa.tobias4b@gmail.com>
2024-09-01 19:16:23 -0600

    add AddCategory component tests

--- tests/components/AddCategory.test.jsx
+++ tests/components/AddCategory.test.jsx
@@ -1,8 +1,11 @@
-import { describe, expect, test } from 'vitest'
-import { fireEvent, render, screen } from '@testing-library/react'
+import { afterEach, describe, expect, test } from 'vitest'
+import { cleanup, fireEvent, render, screen } from '@testing-library/react'
 import { AddCategory } from '../../src/components'
 
 describe('<AddCategory /> tests', () => { 
+    afterEach(() => {
+        cleanup()
+    })
     
     test('should change input value', () => { 
         render(<AddCategory onNewCategory={() => {}} />)
@@ -11,6 +14,21 @@ describe('<AddCategory /> tests', () => {
         fireEvent.input(input, { target: { value: 'Suguru Geto' }})
 
         expect(input.value).toBe('Suguru Geto')
-        screen.debug()
      })
+
+     test('should call onNewCategory if input has a value', () => { 
+        const inputValue = 'Suguru Geto'
+        // TODO: ????
+
+        render(<AddCategory onNewCategory={() => {}} />)
+        const input = screen.getByRole('textbox')
+        const form = screen.getByRole('form')
+
+        fireEvent.input(input, { target: { value: inputValue }})
+        fireEvent.submit(form)
+
+        expect(input.value).toBe('')
+
+        screen.debug()
+      })
  })
